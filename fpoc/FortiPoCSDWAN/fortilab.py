97c500615a73a7dd2e57df20f00f64c1b439176a
shamelin <stephane.hamelin06@gmail.com>
2024-09-17 19:12:19 +0200

    PoC11 - HW Lab (cont.)

--- fpoc/FortiPoCSDWAN/fortilab.py
+++ fpoc/FortiPoCSDWAN/fortilab.py
@@ -26,21 +26,21 @@ SDW_1001F_B = FortiGate(name_fpoc='SDW-1001F-B', alias='SDW-1001F-B', model='FGT
 SDW_3301E_A = FortiGate(name_fpoc='SDW-3301E-A', alias='SDW-3301E-A', model='FGT_3301E', password='fortinet',
                             npu='NP6', reboot_delay=300,
                             mgmt=Interface('mgmt1', 0, '10.210.0.63/23'),
-                            lan=Interface('port5', vlanid=0, speed='1000auto'),
+                            lan=Interface('port22', vlanid=0, speed='1000full'),
                             wan=WAN(
-                                inet1=Interface('port1', vlanid=0, speed='1000auto'),
-                                inet2=Interface('port2', vlanid=0, speed='1000auto'),
-                                mpls1=Interface('port4', vlanid=0, speed='1000auto'),
+                                inet1=Interface('port17', vlanid=0, speed='1000full'),
+                                inet2=Interface('port18', vlanid=0, speed='1000full'),
+                                mpls1=Interface('port20', vlanid=0, speed='1000full'),
                             ))
 
 SDW_3301E_B = FortiGate(name_fpoc='SDW-3301E-B', alias='SDW-3301E-B', model='FGT_3301E', password='fortinet',
                             npu='NP6', reboot_delay=300,
                             mgmt=Interface('mgmt1', 0, '10.210.0.67/23'),
-                            lan=Interface('port5', vlanid=0, speed='1000auto'),
+                            lan=Interface('port22', vlanid=0, speed='1000full'),
                             wan=WAN(
-                                inet1=Interface('port1', vlanid=0, speed='1000auto'),
-                                inet2=Interface('port2', vlanid=0, speed='1000auto'),
-                                mpls1=Interface('port4', vlanid=0, speed='1000auto'),
+                                inet1=Interface('port17', vlanid=0, speed='1000full'),
+                                inet2=Interface('port18', vlanid=0, speed='1000full'),
+                                mpls1=Interface('port20', vlanid=0, speed='1000full'),
                             ))
 
 SDW_101F_A = FortiGate(name_fpoc='SDW-101F-A', alias='SDW-101F-A', model='FGT_101F', password='fortinet',
