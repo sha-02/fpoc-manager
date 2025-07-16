f05722e241c171b2b66daf35e7d3d0d7dd4d27e6
shamelin <stephane.hamelin06@gmail.com>
2025-07-16 16:18:43 +0200

    PoC9+10 - Fix for using Fabric Studio

--- fpoc/FortiPoCSDWAN/sdwan1.py
+++ fpoc/FortiPoCSDWAN/sdwan1.py
@@ -6,7 +6,7 @@ import copy
 
 import fpoc
 from fpoc.devices import Interface, FortiGate, LXC
-from fpoc.FortiPoCSDWAN import FortiPoCSDWAN, FortiLabSDWAN
+from fpoc.FortiPoCSDWAN import FortiPoCSDWAN, FortiLabSDWAN, FabricStudioSDWAN
 from fpoc.typing import TypePoC
 import typing
 
@@ -48,6 +48,8 @@ def dualdc(request: WSGIRequest) -> HttpResponse:
     # Create the poc
     if 'fortipoc' in request.path:  # poc is running in FortiPoC
         poc = FortiPoCSDWAN(request)
+    elif 'fabric'  in request.path:  # poc is running in FabricStudio
+        poc = FabricStudioSDWAN(request)
     else:  # poc is running in Hardware Lab
         poc = FortiLabSDWAN(request)
 
